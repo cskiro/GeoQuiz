476f69b601b1d3d222aba40f6f9c889eb46b433e
cskiro <skiroc@ELSPHIM-221373.science.regn.net>
2017-01-12 19:02:12 -0500

    Getting references to True/False buttons and Setting OnClickListeners to them

--- app/src/main/java/com/example/skiroc/geoquiz_v20/QuizActivity.java
+++ app/src/main/java/com/example/skiroc/geoquiz_v20/QuizActivity.java
@@ -2,12 +2,36 @@ package com.example.skiroc.geoquiz_v20;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class QuizActivity extends AppCompatActivity {
+
+    private Button mTrueButton;
+    private Button mFalseButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_quiz);
+
+        // Getting references to Widgets
+        mTrueButton = (Button) findViewById(R.id.true_button);
+        mFalseButton = (Button) findViewById(R.id.false_button);
+
+        // Setting OnClick Listener for True/False Buttons
+        mTrueButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+        mFalseButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                
+            }
+        });
     }
 }
