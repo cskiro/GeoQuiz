32172682d943171242a9b557511bb2df3d885aa6
cskiro <skiroc@ELSPHIM-221373.science.regn.net>
2017-01-12 19:55:43 -0500

    Wiring up Question TextView with questions

--- app/src/main/java/com/example/skiroc/geoquiz_v20/QuizActivity.java
+++ app/src/main/java/com/example/skiroc/geoquiz_v20/QuizActivity.java
@@ -4,12 +4,29 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class QuizActivity extends AppCompatActivity {
 
     private Button mTrueButton;
     private Button mFalseButton;
+    private Button mNextButton;
+    private TextView mQuestionTextView;
+
+    /**
+     * Calling the Question constructor to create
+     * an array of Question objects
+     */
+    private Question[] mQuestionBank = new Question[] {
+            new Question(R.string.question_oceans, true),
+            new Question(R.string.question_mideast, false),
+            new Question(R.string.question_africa, false),
+            new Question(R.string.question_americas, true),
+            new Question(R.string.question_asia, true),
+    };
+
+    private int mCurrentIndex = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +36,11 @@ public class QuizActivity extends AppCompatActivity {
         // Getting references to Widgets
         mTrueButton = (Button) findViewById(R.id.true_button);
         mFalseButton = (Button) findViewById(R.id.false_button);
+        mQuestionTextView = (TextView) findViewById(R.id.question_text_view);
+
+        // Setting Question TextView text to current question
+        int question = mQuestionBank[mCurrentIndex].getTextResId();
+        mQuestionTextView.setText(question);
 
         // Setting OnClick Listener for True/False Buttons
         mTrueButton.setOnClickListener(new View.OnClickListener() {
