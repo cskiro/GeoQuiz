8bd6c6284b32a2d5d5230e6c4e08bbab35f5638d
cskiro <skiroc@ELSPHIM-221373.science.regn.net>
2017-01-12 19:25:18 -0500

    Creating Question class

---
+++ app/src/main/java/com/example/skiroc/geoquiz_v20/Question.java
@@ -0,0 +1,11 @@
+package com.example.skiroc.geoquiz_v20;
+
+/**
+ * Created by skiroc on 1/12/17.
+ */
+
+public class Question {
+
+    private int mTextResId;
+    private boolean mIsAnswerTrue;
+}
