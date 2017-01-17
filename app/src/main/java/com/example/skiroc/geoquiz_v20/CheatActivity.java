cedeb6b68e5a4fa506637d548c9674a81fbef09d
cskiro <skiroc@ELSPHIM-221373.science.regn.net>
2017-01-17 09:11:30 -0500

    Adding extra constant and new Intent

--- app/src/main/java/com/example/skiroc/geoquiz_v20/CheatActivity.java
+++ app/src/main/java/com/example/skiroc/geoquiz_v20/CheatActivity.java
@@ -1,9 +1,24 @@
 package com.example.skiroc.geoquiz_v20;
 
+import android.content.Context;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class CheatActivity extends AppCompatActivity {
+
+    private static final String EXTRA_ANSWER_IS_TRUE = "com.example.skiroc.geoquiz_v20.answer_is_true";
+
+    public static Intent newIntent(Context packageContext, boolean answerIsTrue) {
+
+        Intent intent = new Intent(packageContext, CheatActivity.class);
+        /**
+         * @param key String
+         * @param value boolean
+         */
+        intent.putExtra(EXTRA_ANSWER_IS_TRUE, answerIsTrue);
+        return intent;
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
