827a49be2847cc3c0533166d5a291208087d37c1
cskiro <skiroc@ELSPHIM-221373.science.regn.net>
2017-01-23 13:27:12 -0500

    Checking device's build version

--- app/src/main/java/com/example/skiroc/geoquiz_v20/CheatActivity.java
+++ app/src/main/java/com/example/skiroc/geoquiz_v20/CheatActivity.java
@@ -4,6 +4,7 @@ import android.animation.Animator;
 import android.animation.AnimatorListenerAdapter;
 import android.content.Context;
 import android.content.Intent;
+import android.os.Build;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -62,19 +63,26 @@ public class CheatActivity extends AppCompatActivity {
                 }
                 setAnswerShownResult(true);
 
-                int cx = mShowAnswerButton.getWidth() / 2;
-                int cy = mShowAnswerButton.getHeight() / 2;
-                float radius = mShowAnswerButton.getWidth();
-                Animator animator = ViewAnimationUtils
-                        .createCircularReveal(mShowAnswerButton, cx, cy, radius, 0);
-                animator.addListener(new AnimatorListenerAdapter() {
-                    @Override
-                    public void onAnimationEnd(Animator animation) {
-                        super.onAnimationEnd(animation);
-                        mShowAnswerButton.setVisibility(View.INVISIBLE);
-                    }
-                });
+
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+                    int cx = mShowAnswerButton.getWidth() / 2;
+                    int cy = mShowAnswerButton.getHeight() / 2;
+                    float radius = mShowAnswerButton.getWidth();
+                    Animator animator = ViewAnimationUtils
+                            .createCircularReveal(mShowAnswerButton, cx, cy, radius, 0);
+                    animator.addListener(new AnimatorListenerAdapter() {
+                        @Override
+                        public void onAnimationEnd(Animator animation) {
+                            super.onAnimationEnd(animation);
+                            mShowAnswerButton.setVisibility(View.INVISIBLE);
+                        }
+                    });
+                    animator.start();
+                } else {
+                    mShowAnswerButton.setVisibility(View.INVISIBLE);
+                }
             }
+
         });
     }
 
